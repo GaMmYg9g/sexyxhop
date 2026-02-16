963815b8b403e736d90c589092174662e77a5e2c
GaMmYg9g <gammytheninth@gmail.com>
2026-02-16 00:16:14 -0500

    Delete api directory

--- api/get-reviews.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-require_once '../database/db.php';
-
-$id = $_GET['id'] ?? 0;
-$stmt = $db->prepare("SELECT * FROM reviews WHERE product_id = ? ORDER BY created_at DESC");
-$stmt->execute([$id]);
-echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
-?>
